c977ffebfbec52e982d1a30e9b1d9f08e1893f5d
Anjali Pare <anjalipare@Anjalis-MacBook-Air.local>
2019-10-10 17:30:26 -0500

    Got the reset button to work

---
+++ formChecker.js
@@ -0,0 +1,12 @@
+function reset()
+{
+  document.getElementById("cookie").value = null;
+  document.getElementById("ccd").value = null;
+  document.getElementById("ccc").value = null;
+  document.getElementById("pass").value = null;
+  document.getElementById("uname").value = null;
+  document.getElementById("Free").checked = false;
+  document.getElementById("Overnight").checked = false;
+  document.getElementById("3-day").checked = false;
+
+}
