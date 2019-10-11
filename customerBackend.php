e06fe426ad558d5dadec604efdde8738fe0ace4a
Anjali Pare <anjalipare@host-240-51.kslaccn.lawrence.ks.us.clients.pavlovmedia.net>
2019-10-10 23:17:55 -0500

    Changed some things in the php file and got the styling to work

--- customerBackend.php
+++ customerBackend.php
@@ -4,22 +4,27 @@ $cookie = $_POST["cookie"];
 $ccd = $_POST["ccd"];
 $ccc = $_POST["ccc"];
 $ship = $_POST["ship"];
+$passw = $_POST["password"];
 
 $sum = ($cookie * 2)+($ccd * 3)+($ccc * 7);
 if($ship == "overnight")
 {
   $sum = $sum + 50;
-  $Cost = 50;
+$Cost = 50;
 }
 else if($ship == "days3")
 {
   $sum = $sum + 5;
-  $Cost = 5;
+$Cost = 5;
 }
-
+else
+{
+$Cost = 0;
+}
 echo "<br>";
-echo "Thank you for your purchase. Here is your receipt<br><br>";
-echo "<table border = 1 style = \"width:30%\">";
+echo "<p>Thank you for your purchase. Here is your receipt</p><br>";
+echo "<p>This is your password: " . $passw . "</p><br>";
+echo "<table border = 1 style = \"width:40%\">";
 echo "<tr>";
 echo "<th> </th>";
 echo "<th>Quantity</th> <th>Cost Per Item</th> <th>Sub Total</th></tr>";
@@ -39,9 +44,16 @@ echo ("<td>$" . $ccc . "</td>");
 echo ("<td>$7.00</td>");
 echo ("<td>$" . $ccc * 7 . "</td>");
 echo "</tr><tr>";
-echo "<th>Shipping</th><td colspan = 2>" . $ship . "</td><td>$" . $Cost . "</td></tr><tr>";
+if($ship == "days3")
+{
+  echo "<th>Shipping</th><td colspan = 2>3-day</td><td>$" . $Cost . "</td></tr><tr>";
+}
+else
+{
+ echo "<th>Shipping</th><td colspan = 2>" . $ship . "</td><td>$" . $Cost . "</td></tr><tr>";
+}
 echo "<th colspan = 3 >Total Cost</th>";
-echo ("<td>$" . $sum . "</td>");
-echo "</tr>"
+echo ("<th>$" . $sum . "</th>");
+echo "</tr>";
 echo "</table>";
 ?>
